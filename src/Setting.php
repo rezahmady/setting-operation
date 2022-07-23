e3f734eaad561503292d623314621e10d18fcb40
reza ahmadi sabzevar <ahmadireza15@gmail.com>
2022-07-23 22:45:45 +0430

    fixed databese errro

--- src/Setting.php
+++ src/Setting.php
@@ -11,9 +11,11 @@ class Setting
         $key = $arr[0];
         $field = $arr[1];
         $settings = $model::where('key', $key)->first();
-        $fields = json_decode($settings->fields);
-        if(property_exists($fields,$field)) {
-            return $fields->{$field};
+        if($settings) {
+            $fields = json_decode($settings->fields) ?? [];
+            if(property_exists($fields,$field)) {
+                return $fields->{$field};
+            }
         }
         return $default;
     }
